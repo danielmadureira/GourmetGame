50a9c2c63619bba3a206686dcd8da5d06d77a748
Daniel Madureira <danielmadsv@gmail.com>
2020-09-13 03:21:31 -0300

    Cria classe controladora do app
    Cria controlador com sequência de execução do app.

---
+++ src/controller/AppController.js
@@ -0,0 +1,158 @@
+const WelcomeScreen = require("../view/WelcomeScreen")
+const DishGuessScreen = require("../view/DishGuessScreen")
+const SuccessScreen = require("../view/SuccessScreen")
+const CreateDishScreen = require("../view/CreateDishScreen")
+const CreateDishCategoryScreen = require("../view/CreateDishCategoryScreen")
+const Dish = require("../model/Dish")
+/**
+ * Controlador da aplicação.
+ */
+class AppController {
+
+  /**
+   * Construtor do controlador da aplicação.
+   * 
+   * @param { DependencyContainer } config 
+   */
+  constructor(DC) {
+    this.DC = DC
+    this.successCount = 0
+  }
+
+  /**
+   * Executa a aplicação.
+   */
+  async run() {
+    await this.indexPage()
+
+    const dishes = this.DC.store.getStore('dish')
+
+    const [ hasGuessedDish, dishSet ] = await this.guessDish(dishes)
+
+    if (hasGuessedDish) {
+      await this.showSuccess()
+
+    } else {
+      await this.createNewDish(dishSet)
+    }
+
+    this.run()
+  }
+
+  /**
+   * Carrega a página inicial da aplicação.
+   */
+  async indexPage() {
+    const view = new WelcomeScreen(this.DC.userInterface)
+    const userResponse = await view.render()
+
+    if (!userResponse) {
+      this.terminate()
+    }
+  }
+
+  /**
+   * Encerra a execução do app.
+   */
+  terminate() {
+    process.exit(0)
+  }
+
+  /**
+   * Carrega a tela para tentar adivinhar
+   * o prato do usuário.
+   * 
+   * @param { Set } dishSet 
+   * 
+   * @returns { Array }
+   */
+  async guessDish(dishSet) {
+    const view = new DishGuessScreen(this.DC.userInterface)
+
+    let dish
+    for await (dish of dishSet) {
+      const userResponse = await view.render(dish.name)
+
+      if (userResponse) {
+        if (dish.hasDishes()) {
+          return await this.guessDish(dish.dishes)
+        }
+
+        return [ true, null ]
+      }
+    }
+
+    return [ false, dishSet ]
+  }
+
+  /**
+   * Carrega a tela para exibir a mensagem
+   * de sucesso.
+   */
+  async showSuccess() {
+    ++ this.successCount
+
+    const view = new SuccessScreen(this.DC.userInterface)
+
+    await view.render(this.successCount > 1)
+  }
+
+  /**
+   * Carrega as telas para cadastrar um novo prato.
+   * 
+   * @param { Set } dishSet 
+   */
+  async createNewDish(dishSet) {
+    const newDishName = await this._enterNewDish()
+
+    // First item in the set.
+    const lastItemInSet = [ ...dishSet ].pop()
+
+    const newDishCatName = await this._enterNewDishCat(newDishName, lastItemInSet.name)
+
+    const dishCat = new Dish(newDishCatName)
+    dishCat.addDish(new Dish(newDishName))
+
+    dishSet.add(dishCat)
+  }
+
+  /**
+   * Carrega a tela para buscar o nome do prato
+   * que o usuário está pensando.
+   * 
+   * @returns { String }
+   */
+  async _enterNewDish() {
+    const createDish = new CreateDishScreen(this.DC.userInterface)
+    let newDishName = ''
+    do {
+      newDishName = await createDish.render()
+    } while (newDishName === '')
+
+    return newDishName    
+  }
+
+  /**
+   * Carrega tela para buscar o nome do prato
+   * ao qual o prato que o usuário está pensando
+   * pertence.
+   * 
+   * @param { String } newDishName Nome do prato que o usuário está pensando.
+   * @param { String } lastItemInSet Nome do prato do último palpite da aplicação.
+   * 
+   * @returns { String }
+   */
+  async _enterNewDishCat(newDishName, lastItemInSet) {
+    const createDishCat = new CreateDishCategoryScreen(this.DC.userInterface)
+    let newDishCatName = ''
+
+    do {
+      newDishCatName = await createDishCat.render(newDishName, lastItemInSet)
+    } while (newDishCatName === '')
+    
+    return newDishCatName
+  }
+
+}
+
+module.exports = AppController
