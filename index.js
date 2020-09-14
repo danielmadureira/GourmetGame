109724159655b16d046488121c3e7e29ce2e5fa3
Daniel Madureira <danielmadsv@gmail.com>
2020-09-14 02:11:32 -0300

    Define CLI como interface de usuário principal

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const DependencyContainer = require('./src/model/DependencyContainer')
 const AppController = require('./src/controller/AppController')
-const GUIUserInterface = require('./src/model/UI/GUIUserInterface')
+const CLIUserInterface = require('./src/model/UI/CLIUserInterface')
 
 /* Bootstrapping the app. */
 const store = require('./store')
 
 const DC = new DependencyContainer()
-DC.userInterface = new GUIUserInterface()
+DC.userInterface = new CLIUserInterface()
 DC.store = store
 /* - - - */
 
